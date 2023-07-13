22c8eac04e44a782ead68725f37060135a0a74a6
Dimple10 <sarnaaik@usc.edu>
2023-07-13 17:36:04 +0000

    Update config file

---
+++ constraints/pipelines/darkMatter/progenitorMassFunctionConfig.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#PBS -N mcmc_mdpl2
+#PBS -l nodes=4:ppn=16
+#PBS -j oe
+#PBS -o mcmc_mdpl2.log
+#PBS -V
+if [ ! -z ${PBS_O_WORKDIR+x} ]; then
+ cd $PBS_O_WORKDIR
+elif [ ! -z ${SLURM_SUBMIT_DIR+x} ]; then
+ cd $SLURM_SUBMIT_DIR
+fi
+
+export LD_LIBRARY_PATH=/home/abenson/Galacticus/Tools/lib:/home/abenson/Galacticus/Tools/lib64:$LD_LIBRARY_PATH
+export PATH=/home/abenson/Galacticus/Tools/bin:$PATH
+export GALACTICUS_FCFLAGS="-fintrinsic-modules-path /home/abenson/Galacticus/Tools/finclude -fintrinsic-modules-path /home/abenson/Galacticus/Tools/include -fintrinsic-modules-path /home/abenson/Galacticus/Tools/include/gfortran -fintrinsic-modules-path /home/abenson/Galacticus/Tools/lib/gfortran/modules -L/home/abenson/Galacticus/Tools/lib -L/home/abenson/Galacticus/Tools/lib64 -L/home/abenson/Galacticus/Tools/lib -L/home/abenson/Galacticus/Tools/lib64"
+export GALACTICUS_CFLAGS="-I/home/abenson/Galacticus/Tools/include"
+export GALACTICUS_CPPFLAGS="-I/home/abenson/Galacticus/Tools/include"
+ulimit -t unlimited
+ulimit -c unlimited
+export OMP_NUM_THREADS=1
+mpirun --n 16 --map-by node --bind-to none -mca btl ^openib ./Galacticus.exe constraints/pipelines/darkMatter/progenitorMassFunctionConfig.xml
+exit
