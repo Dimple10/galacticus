e57bd37b4aa682a40ebbbcf0b74760a296d6748e
Dimple Sarnaaik <sarnaaik@usc.edu>
2026-02-24 13:52:47 -0800

    Changes to base files for updated likelihood runs

---
+++ constraints/pipelines/darkMatter/progenitorMassFunctionConfig_MDPLSymAllth_WDMallsk_restart.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -N th_MDPLSymAll_WDMallsk
+#PBS -l nodes=8:ppn=8
+#PBS -j oe
+#PBS -o MDPLSymAllth_WDMallsk.log
+#SBATCH --mem-per-cpu=8G
+#PBS -m ea
+#PBS -M dsarnaaik@carnegiescience.edu
+#PBS -V
+if [ ! -z ${PBS_O_WORKDIR+x} ]; then
+ cd $PBS_O_WORKDIR
+elif [ ! -z ${SLURM_SUBMIT_DIR+x} ]; then
+ cd $SLURM_SUBMIT_DIR
+fi
+
+export LD_LIBRARY_PATH=/home/abenson/Galacticus/Tools/lib:/home/abenson/Galacticus/Tools/lib64:$LD_LIBRARY_PATH
+export PATH=/home/abenson/Galacticus/Tools/bin:$PATH
+export GALACTICUS_FCFLAGS="-fintrinsic-modules-path /home/abenson/Galacticus/Tools/finclude -fintrinsic-modules-path /home/abenson/Galacticus/Tools/include -fintrinsic-modules-path /home/abenson/Galacticus/Tools/include/gfortran -fintrinsic-modules-path /home/abenson/Galacticus/Tools/lib/gfortran/modules -L/home/abenson/Galacticus/Tools/lib -L/home/abenson/Galacticus/Tools/lib64 -L/home/abenson/Galacticus/Tools/lib -L/home/abenson/Galacticus/Tools/lib64"
+export GALACTICUS_CFLAGS="-I/home/abenson/Galacticus/Tools/include"
+export GALACTICUS_CPPFLAGS="-I/home/abenson/Galacticus/Tools/include"
+ulimit -t unlimited
+ulimit -c unlimited
+export OMP_NUM_THREADS=1
+/usr/bin/time -v mpirun --n 64 --map-by node --bind-to none -mca btl ^openib ./Galacticus.exe constraints/pipelines/darkMatter/progenitorMassFunctionConfig_MDPLSymAllth_WDMallsk_restart.xml
+exit
